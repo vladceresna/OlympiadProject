15c808d0641b6d9d854f2ece569cc407881790e7
Vlad Ĉereŝna <vladceresna6@gmail.com>
2024-02-17 10:12:27 +0200

    Task A

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int c = s.nextInt();
+        System.out.println((a-c)+(b-c)+c);
     }
 }
