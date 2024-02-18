80d97f6c3eb001e6ff10fd3f20d3efd20a0f3b3e
Vlad Ĉereŝna <vladceresna6@gmail.com>
2024-02-18 10:16:33 +0200

    Task A

--- src/com/example/example/Main.java
+++ src/com/example/example/Main.java
@@ -1,7 +1,11 @@
 package com.example.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        System.out.println((n*20-19)+" "+(n*20));
     }
 }
